0da79480a1c663512c657a8ac811ccf61209240f
DLehenbauer <DLehenbauer@users.noreply.github.com>
2020-05-05 21:23:53 -0700

    Cycle 56-63

--- tools/cycle/cycle.ts
+++ tools/cycle/cycle.ts
@@ -61,7 +61,7 @@ function meetsThreshold(_cycle: ICycleInfo) {
 const [,, partitionSizeArg, partitionAssignmentArg] = process.argv;
 
 const partitionSize = parseInt(partitionSizeArg);
-const partitionAssignment = parseInt(partitionAssignmentArg) + (8 * 6);
+const partitionAssignment = parseInt(partitionAssignmentArg) + (8 * 7);
 const minM = (partitionSize * partitionAssignment) | 1;
 const maxM = minM + partitionSize - 1;
 
