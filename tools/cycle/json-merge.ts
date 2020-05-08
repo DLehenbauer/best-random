53781fd48cbb334557d1c98903781b6af15429b9
DLehenbauer <DLehenbauer@users.noreply.github.com>
2020-05-08 10:33:39 -0700

    Cycle 80-87

--- tools/cycle/json-merge.ts
+++ tools/cycle/json-merge.ts
@@ -3,7 +3,7 @@ import { hex } from "../utils";
 
 let a: any[] = [];
 
-for (let i = 0; i <= 79; i++) {
+for (let i = 0; i <= 87; i++) {
     const filename = `cycle-${i}.json`;
     console.log(`${filename}:`)
     a = a.concat(JSON.parse(`[${fs.readFileSync(filename).toString().trim().slice(0, -1)}]`));
@@ -11,6 +11,6 @@ for (let i = 0; i <= 79; i++) {
 
 a.sort((left: any, right: any) => right.len - left.len);
 
-for (const e of a.slice(0, 100)) {
+for (const e of a.slice(0, 25)) {
     console.log(`    // a -= rot(${e.M} * b, ${e.C1});`.padEnd(30), `// len=${hex(e.len)} ${((e.len/0x100000000) * 100).toFixed(2)}%`);
 }
