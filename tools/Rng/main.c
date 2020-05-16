5d5c1b363cff667ef88c3d77333018dc4bf200f6
DLehenbauer <DLehenbauer@users.noreply.github.com>
2020-05-16 09:49:15 -0700

    Test: Add 'TestU01_rng'

---
+++ tools/Rng/main.c
@@ -0,0 +1,77 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include "rng.h"
+
+#define ELEMENT_COUNT 255
+#define BUFFER_SIZE (sizeof(uint32_t) * ELEMENT_COUNT)
+
+static bool emitHi = true;
+static bool emitLo = true;
+static bool reverse = false;
+
+uint32_t reverse32(uint32_t v) {
+    v = ((v >> 1) & 0x55555555) | ((v & 0x55555555) << 1);
+    v = ((v >> 2) & 0x33333333) | ((v & 0x33333333) << 2);
+    v = ((v >> 4) & 0x0F0F0F0F) | ((v & 0x0F0F0F0F) << 4);
+    v = ((v >> 8) & 0x00FF00FF) | ((v & 0x00FF00FF) << 8);
+    v = ( v >> 16             ) | ( v               << 16);
+
+    return v;
+}
+
+bool parseArg(char* arg) {
+    if (strcmp(arg, "-h") == 0)   { emitLo = false; return emitHi; }
+    if (strcmp(arg, "-l") == 0)   { emitHi = false; return emitLo; }
+    if (strcmp(arg, "-r") == 0)   { reverse = true; return true; }
+
+    return false;
+}
+
+static inline void write(uint32_t value) {
+    static uint32_t buffer[ELEMENT_COUNT];
+    static int i = 0;
+
+    if (i == ELEMENT_COUNT) {
+        fwrite((void*) &buffer, sizeof(uint32_t), ELEMENT_COUNT, stdout);
+        i = 0;
+    }
+
+    buffer[i++] = value;
+}
+
+int main(int argc, char *argv[]) {
+    FILE* fp = freopen(NULL, "wb", stdout);  // Only necessary on Windows, but harmless.
+    assert(fp);
+
+    for (int i = 1; i < argc; i++) {
+        if (!parseArg(argv[i])) {
+            fprintf(stderr, "%s [-h] [-l] [-r]\n", argv[0]);
+            fprintf(stderr, "    -h = high 32bits only\n");
+            fprintf(stderr, "    -l = low 32bits only\n");
+            fprintf(stderr, "    -r = reverse bits\n");
+            return 1;
+        }
+    }
+
+    rng_init();
+
+    while (1) {
+        uint32_t value;
+
+        if (emitLo) {
+            value = rng_u32l();
+            if (reverse) { value = reverse32(value); }
+            write(value);
+        }
+
+        if (emitHi) {
+            value = rng_u32h();
+            if (reverse) { value = reverse32(value); }
+            write(value);
+        }
+    }
+
+    return 0;
+}
--- tools/Rng/rng.c
+++ tools/Rng/rng.c
@@ -1,76 +1,62 @@
-#include <assert.h>
-#include <stdio.h>
-#include <string.h>
 #include "rng.h"
+#include <stdio.h>
+#include <sys/time.h>
 
-#define ELEMENT_COUNT 255
-#define BUFFER_SIZE (sizeof(uint32_t) * ELEMENT_COUNT)
+static uint32_t x = 0;
+static uint32_t y = 0;
+static uint32_t z = 0;
+static uint32_t w = 0;
 
-static bool emitHi = true;
-static bool emitLo = true;
-static bool reverse = false;
+static inline uint32_t rot(uint32_t v, uint32_t k) { k &= 31; return (v << k) | (v >> (32 - k)); }
 
-uint32_t reverse32(uint32_t v) {
-    v = ((v >> 1) & 0x55555555) | ((v & 0x55555555) << 1);
-    v = ((v >> 2) & 0x33333333) | ((v & 0x33333333) << 2);
-    v = ((v >> 4) & 0x0F0F0F0F) | ((v & 0x0F0F0F0F) << 4);
-    v = ((v >> 8) & 0x00FF00FF) | ((v & 0x00FF00FF) << 8);
-    v = ( v >> 16             ) | ( v               << 16);
+void advance() {
+    uint32_t t = x;
+    x = y;
+    y = z;
+    z = w;
 
-    return v;
+    t ^= t << 15;   // x (lo 17b)  0fedcba9 87654321 0....... ........
+    t ^= t >> 18;   // x (hi 14b)  ........ ........ ..fedcba 98765432
+    t ^= w << 11;   // w (lo 21b)  43210fed cba98765 43210... ........           
+
+    w = t;
 }
 
-bool parseArg(char* arg) {
-    if (strcmp(arg, "-h") == 0)   { emitLo = false; return emitHi; }
-    if (strcmp(arg, "-l") == 0)   { emitHi = false; return emitLo; }
-    if (strcmp(arg, "-r") == 0)   { reverse = true; return true; }
-
-    return false;
+uint32_t mix(uint32_t a, uint32_t b) {
+    uint32_t c = b >> 28;
+    a ^= a >> (c + 5);
+    a += rot(b * 16777619, 19);
+    a ^= a >> (c + 13);
+    return a;
 }
 
-static inline void write(uint32_t value) {
-    static uint32_t buffer[ELEMENT_COUNT];
-    static int i = 0;
+uint32_t hi32() { return mix(x - z, y - w); }
+uint32_t lo32() { return mix(w - y, z - x); }
 
-    if (i == ELEMENT_COUNT) {
-        fwrite((void*) &buffer, sizeof(uint32_t), ELEMENT_COUNT, stdout);
-        i = 0;
-    }
+uint32_t rng_u32h() { advance(); return hi32(); }
+uint32_t rng_u32l() { advance(); return lo32(); }
 
-    buffer[i++] = value;
+uint64_t rng_u64() {
+    advance();
+    
+    uint64_t t = hi32();
+    t <<= 32;
+    return t | lo32();
 }
 
-int main(int argc, char *argv[]) {
-    FILE* fp = freopen(NULL, "wb", stdout);  // Only necessary on Windows, but harmless.
-    assert(fp);
+uint32_t us() {
+    struct timeval start;
+    gettimeofday(&start, NULL);
+    return start.tv_sec * 1000000 + start.tv_usec;
+}
 
-    for (int i = 1; i < argc; i++) {
-        if (!parseArg(argv[i])) {
-            fprintf(stderr, "%s [-h] [-l] [-r]\n", argv[0]);
-            fprintf(stderr, "    -h = high 32bits only\n");
-            fprintf(stderr, "    -l = low 32bits only\n");
-            fprintf(stderr, "    -r = reverse bits\n");
-            return 1;
-        }
-    }
+void rng_init() {
+    x = y = z = w = (us() | 1);
 
-    while (1) {
+    for (int i = 0; i < 7; i++) {
+        w = mix(hi32(), lo32());
         advance();
-
-        uint32_t value;
-
-        if (emitLo) {
-            value = lo32();
-            if (reverse) { value = reverse32(value); }
-            write(value);
-        }
-
-        if (emitHi) {
-            value = hi32();
-            if (reverse) { value = reverse32(value); }
-            write(value);
-        }
     }
 
-    return 0;
+    printf("seed: %x %x %x %x\n", x, y, z, w);
 }
