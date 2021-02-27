93c0a4e11e893a638e46b433961ef75c685f5af9
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2021-02-27 22:10:47 +0000

    x3-hack

--- experiments/x3/run.sh
+++ experiments/x3/run.sh
@@ -9,27 +9,28 @@ rng="../../../../../Rng/rng"
 run () {
     name=$1
     size=$2
-    filter=$3
 
     rm -rf $logDir
     mkdir $logDir \
         && cat $argsFile | parallel --colsep ' ' --workDir $workDir "mkdir '$logDir/{1}-{2}' && $rng -p0 {1} -p1 {2} | stdbuf -oL -eL ./mod3 $size > '$logDir/{1}-{2}/report.txt'" \
         && node p.js > report-mod3-$name.json \
-        && cat report-mod3-$name.json | node f$filter.js > $argsFile
+        && cat report-mod3-$name.json | node f.js > $argsFile
 }
 
-rm $argsFile
-rm report-*.json
-for i in {0..31}
-do
-    for j in {0..31}
-    do
-        echo "$i $j" >> $argsFile
-    done
-done
+# rm $argsFile
+# rm report-*.json
+# for i in {0..31}
+# do
+#     for j in {0..31}
+#     do
+#         echo "$i $j" >> $argsFile
+#     done
+# done
 
-run tiny 10485760 1 \
-    && run small 104857600 1 \
-    && run standard 1073741824 1 \
-    && run big 10737418240 1 \
-    && run huge 107374182400 1
+# run tiny 10485760 \
+#     && run small 104857600 \
+#     && run standard 1073741824 \
+#     && run big 10737418240 \
+#     && run huge 107374182400 \
+    run tera 1099511627776 \
+    && run ten-tera 10995116277760
