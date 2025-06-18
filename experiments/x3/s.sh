0ef59cf2f0ddbe82885df19c08b9898b01362329
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2025-06-18 08:42:11 -0700

    show scan stats

--- experiments/x3/s.sh
+++ experiments/x3/s.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
 update () {
+    previous_args_file="pass.bak"
+
+    total=$(cat $previous_args_file | wc -l)
     lastPassed=$(tail -n 1 pass)
-    processed=$(grep -n "^$lastPassed$" pass.bak | cut -d : -f 1)
+    processed=$(grep -n "^$lastPassed$" $previous_args_file | cut -d : -f 1)
+    percent_done=$(echo "scale=2; ($processed * 100) / $total" | bc)
+
     passed=$(cat pass | wc -l)
     rejected=$(echo "scale=2; $processed - $passed" | bc)
-    percent=$(echo "scale=2; 100 - ($passed * 100) / $processed" | bc)
-    total=$(cat args | wc -l)
-    done=$(echo "scale=2; ($processed * 100) / $total" | bc)
+    percent_passed=$(echo "scale=2; 100 - ($passed * 100) / $processed" | bc)
 
     echo
     date
-    echo "$lastPassed -- passed: $passed processed: $processed ($done%) rejected: $rejected ($percent%)"
+    echo "$lastPassed -- processed: $processed/$total ($percent_done%) rejected: $rejected/$processed ($percent_passed%)"
+    echo
+    node ./scan.js
 }
 
 # Update the display once every 'n' seconds
