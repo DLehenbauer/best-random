56bdc80188df72b912f0b8698baf956e2ac8285c
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2025-08-19 06:38:10 -0700

    Add adaptive crush to TestU01_stdin.c

--- tools/TestU01/TestU01_stdin.c
+++ tools/TestU01/TestU01_stdin.c
@@ -7,6 +7,7 @@
 #include <string.h>
 #include <time.h>
 #include "TestU01.h"
+#include "adaptive/adaptive_crush.h"
 #include "common.h"
 
 #define ELEMENT_COUNT 255
@@ -15,14 +16,16 @@
 int usage(char *name)
 {
     printf("\n");
-    printf("Usage: <PRNG_output> | %s [-s] [-c] [-b] [-h] [-l] [-r] [-v]\n", name);
+    printf("Usage: %s [-s|-c|-b] [-h] [-l] [-r] [-v] [-x <loopcount>]\n", name);
     printf("  -s = SmallCrush\n");
     printf("  -c = Crush\n");
     printf("  -b = BigCrush\n");
+    printf("  -a = AdaptiveCrush\n");
     printf("  -h = high32 bits\n");
     printf("  -l = low32 bits\n");
     printf("  -r = reverse bits\n");
     printf("  -v = verbose\n");
+    printf("  -x = loop\n");
     return 1;
 }
 
@@ -100,52 +103,190 @@ unif01_Gen *createGenerator()
     }
 }
 
+uint32_t remainingIterations = 1;
+double testToTotalConfidence[201] = {0};
+uint32_t testToTotalFailures[201] = {0};
+double totalIterationsRun = 0;
+double totalIterationsFailed = 0;
+double totalCasesRun = 0;
+double totalCasesUnusual = 0;
+double totalCasesSuspicious = 0;
+double totalCasesFailed = 0;
+
+// Mirrors two-sided p-values to the left side.  The result is a number in the range [0, 0.5],
+// with 0.5 being the most probable.
+double computePValueDelta(double pVal) {
+    return pVal < 0.5
+        ? pVal
+        : 1.0 - pVal;
+}
+
+double getPValueDelta(uint32_t index) {
+    return computePValueDelta(bbattery_pVal[index]);
+}
+
+// Given a two-sided p-value, returns the confidence score.
+// The result is a number in the range [0, 1], with 1 being the most confident.
+double computeConfidence(double pvalue) {
+    return 2.0 * computePValueDelta(pvalue);
+}
+
+double getConfidence(uint32_t index) {
+    return computeConfidence(bbattery_pVal[index]);
+}
+
+double getAvgConfidence(uint32_t index) {
+    return testToTotalConfidence[index] / totalIterationsRun;
+}
+
 struct sorted_result_t
 {
-    char *name;
-    double delta;
+    uint32_t index;
 };
 
-static int sorted_result_cmp(const void *left, const void *right)
+static int sorted_result_cmp_avg(const void *left, const void *right)
 {
     const struct sorted_result_t *leftResult = left, *rightResult = right;
 
-    return leftResult->delta < rightResult->delta
+    double leftAvg  = getAvgConfidence(leftResult->index);
+    double rightAvg = getAvgConfidence(rightResult->index);
+
+    return leftAvg < rightAvg
                ? 1
-               : leftResult->delta > rightResult->delta
+               : leftAvg > rightAvg
                      ? -1
                      : 0;
 }
 
-bool success()
+static int sorted_result_cmp_delta(const void *left, const void *right)
 {
-    struct sorted_result_t *results = malloc(bbattery_NTests * sizeof(struct sorted_result_t));
+    const struct sorted_result_t *leftResult = left, *rightResult = right;
+
+    double leftConfidence  = getConfidence(leftResult->index);
+    double rightConfidence = getConfidence(rightResult->index);
+
+    return leftConfidence < rightConfidence
+               ? 1
+               : leftConfidence > rightConfidence
+                     ? -1
+                     : 0;
+}
+
+unif01_Gen *gen = NULL;
+
+void displayResults(char* name, struct sorted_result_t* results, bool all) {
+    int numFailed     = 0;
+    int numSuspicious = 0;
+    int numUnusual    = 0;
+
+    bool printHeader = true;
+
     for (int i = 0; i < bbattery_NTests; i++)
     {
-        results[i].name = bbattery_TestNames[i];
-        results[i].delta = bbattery_pVal[i] < 0.5
-                               ? bbattery_pVal[i]
-                               : 1.0 - bbattery_pVal[i];
-    }
+        uint32_t index = results[i].index;
+        double confidence   = getConfidence(index);
+        double delta        = getPValueDelta(index);
+        bool testFailed     = delta < gofw_Suspectp;
+        bool testSuspicious = !testFailed && delta < 0.0025;
+        bool testUnusual    = !testSuspicious && delta < 0.01;
 
-    qsort(results, bbattery_NTests, sizeof *results, sorted_result_cmp);
-
-    for (int i = 0; i < bbattery_NTests; i++)
-    {
-        printf("     %-30s %f%s\n", results[i].name, results[i].delta, results[i].delta < gofw_Suspectp ? "  FAIL!" : "");
-    }
-
-    for (uint32_t j = 0; j < bbattery_NTests; j++)
-    {
-        if ((bbattery_pVal[j] < gofw_Suspectp) || (bbattery_pVal[j] > 1.0 - gofw_Suspectp))
+        if (testFailed)
         {
-            return false;
+            testToTotalFailures[index]++;
+            numFailed++;
         }
+        else if (testSuspicious)
+        {
+            numSuspicious++;    
+        }
+        else if (testUnusual)
+        {
+            numUnusual++;
+        }
+        else if (!swrite_Basic && !all)
+        {
+            continue;
+        }
+
+        if (printHeader) {
+            printf("    avg conf  fail  test                           p-value   confidence\n");
+            printHeader = false;
+        }
+
+        printf("    %f  %4d  %-30s %f  %f %-15s\n",
+               getAvgConfidence(index),
+               testToTotalFailures[index],
+               bbattery_TestNames[index],
+               bbattery_pVal[index],
+               confidence,
+               testFailed
+                   ? "    FAIL!"
+               : testSuspicious
+                   ? "    suspicious"
+               : testUnusual
+                   ? "    unusual"
+                   : "");
     }
 
-    free(results);
+    if (numFailed > 0) {
+        totalIterationsFailed++;
+    }
 
-    return true;
+    totalCasesRun += bbattery_NTests;
+    totalCasesFailed += numFailed;
+    totalCasesSuspicious += numSuspicious;
+    totalCasesUnusual += numUnusual;
+
+    printf("\n%s %s: Passed %d/%d iterations (%.2f%%)%s\n",
+        gen->name,
+        name,
+        (uint32_t) (totalIterationsRun - totalIterationsFailed),
+        (uint32_t) totalIterationsRun,
+        ((totalIterationsRun - totalIterationsFailed) / totalIterationsRun) * 100,
+        numFailed == 0
+            ? ""
+            : " - FAIL!");
+
+    printf("  Total case failure rate: %.2f%% (suspicious: %.2f%%, unusual: %.2f%%)\n",
+        (totalCasesFailed / totalCasesRun) * 100,
+        (totalCasesSuspicious / totalCasesRun) * 100,
+        (totalCasesUnusual / totalCasesRun) * 100);
+}
+
+int run(char* name, void (*battery)(unif01_Gen *gen))
+{
+    totalIterationsRun++;
+
+    gen = createGenerator(arg_hi, arg_lo, arg_rev);
+
+    if (gen == NULL)
+    {
+        fprintf(stderr, "Error: Must specify high (-h) or low (-l)\n");
+        return 1;
+    }
+
+    battery(gen);
+    
+    struct sorted_result_t* results = malloc(bbattery_NTests * sizeof(struct sorted_result_t));
+    
+    for (int i = 0; i < bbattery_NTests; i++)
+    {
+        results[i].index = i;
+        testToTotalConfidence[i] += computeConfidence(bbattery_pVal[i]);
+    }
+
+    qsort(results, bbattery_NTests, sizeof *results, sorted_result_cmp_delta);
+    displayResults(name, results, /* all: */ false);
+
+    // If this is the last iteration resort the results by avg-p and display all tests.
+    if (remainingIterations == 1) {
+        qsort(results, bbattery_NTests, sizeof *results, sorted_result_cmp_avg);
+        displayResults(name, results, /* all: */ true);
+    }
+
+    free(results);    
+    unif01_DeleteExternGenBits(gen);
+    return 0;
 }
 
 int main(int argc, char *argv[])
@@ -154,6 +295,7 @@ int main(int argc, char *argv[])
     bool arg_small = false;
     bool arg_medium = false;
     bool arg_big = false;
+    bool arg_adaptive = false;
 
     for (int i = 1; i < argc; i++)
     {
@@ -168,6 +310,10 @@ int main(int argc, char *argv[])
         else if (strcmp(argv[i], "-b") == 0)
         {
             arg_big = true;
+        }
+        else if (strcmp(argv[i], "-a") == 0)
+        {
+            arg_adaptive = true;
         }
         else if (strcmp(argv[i], "-h") == 0)
         {
@@ -185,35 +331,57 @@ int main(int argc, char *argv[])
         {
             swrite_Basic = TRUE;
         }
+        else if (strcmp(argv[i], "-x") == 0)
+        {
+            if (++i < argc)
+            {
+                if (sscanf (argv[i], "%i", &remainingIterations) != 1)
+                {
+                    fprintf(stderr, "\nError: -x must be followed by a loop count, but got non-integer '%s'.\n", argv[i]);
+                    return usage(argv[0]);
+                }
+            }
+            else
+            {
+                fprintf(stderr, "\nError: -x must be followed by a loop count.\n");
+                return usage(argv[0]);
+            }
+        }
         else
         {
-            fprintf(stderr, "Error: Invalid argument '%s'.\n", argv[i]);
+            fprintf(stderr, "\nError: Invalid argument '%s'.\n", argv[i]);
             return usage(argv[0]);
         }
     }
 
     setvbuf(stdin, (char *)NULL, _IOFBF, BUFFER_SIZE); // full buffering mode
-    unif01_Gen *gen = createGenerator();
 
-    if (arg_small)
+    do
     {
-        bbattery_SmallCrush(gen);
+        if (arg_small)
+        {
+            run("SmallCrush", bbattery_SmallCrush);
+        }
+        else if (arg_medium)
+        {
+            run("Crush", bbattery_Crush);
+        }
+        else if (arg_big)
+        {
+            run("BigCrush", bbattery_BigCrush);
+        }
+        else if (arg_adaptive)
+        {
+            run("AdaptiveCrush", bbattery_Adaptive_Crush);
+        }
+        else
+        {
+            fprintf(stderr, "\nError: Must specify a test battery [-s|-c|-b|-a].\n");
+            return usage(argv[0]);
+        }
     }
-    else if (arg_medium)
-    {
-        bbattery_Crush(gen);
-    }
-    else if (arg_big)
-    {
-        bbattery_BigCrush(gen);
-    }
-    else
-    {
-        fprintf(stderr, "Error: No test battery specified\n");
-        return usage(argv[0]);
-    }
+    while ((--remainingIterations) > 0);
 
-    unif01_DeleteExternGenBits(gen);
-
-    return success() ? 0 : 1;
+    printf("\n");
+    return totalIterationsFailed > 0 ? 1 : 0;
 }
