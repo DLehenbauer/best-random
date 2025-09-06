ca8e278bd737558e241120aa63c7be5f35c890ec
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2025-09-06 12:41:06 -0700

    hunts (48 may be suspect)

--- tools/gf2/hunt2.py
+++ tools/gf2/hunt2.py
@@ -5,15 +5,22 @@ from gf2 import XorshiftAnalyzer
 bit_width = 32
 state_size = 4
 
-def test(c0: int, c1: int, c2: int):
+import itertools
+import time
+
+def test(c):
     a = XorshiftAnalyzer(state_size=state_size, bit_width=bit_width)
 
+    # XSAdd
     def next_state_func(s):
+        sh1 = c[0]
+        sh2 = c[1]
+        sh3 = c[2]
+
         t = s[0]
-        t ^= a.shl(t, c0)
-        t ^= a.shr(t, c1)
-        t ^= a.shl(s[3], c2)
-        
+        t ^= a.shl(t, sh1)
+        t ^= a.shr(t, sh2)
+        t ^= a.shl(s[3], sh3)
         s[0] = s[1]
         s[1] = s[2]
         s[2] = s[3]
@@ -22,12 +29,13 @@ def test(c0: int, c1: int, c2: int):
         return s
 
     result = a.check(next_state_func)
-
     return (result.period == a.max_period)
 
 if __name__ == "__main__":
-    for c0 in range(0, bit_width):
-        for c1 in range(0, bit_width):
-            for c2 in range(0, bit_width):
-                if (test(c0, c1, c2)):
-                    print(f"({c0}, {c1}, {c2}): OK")
+    print(f"--- BEGIN: (bit_width={bit_width}, state_size={state_size})", flush=True)
+    start = time.time()
+    for c in itertools.product(range(bit_width), repeat=3):
+        if test(c):
+            print(f"{c}: OK", flush=True)
+    elapsed = time.time() - start
+    print(f"--- END: (elapsed: {elapsed:.2f} seconds)", flush=True)
--- tools/gf2/hunt49.py
+++ tools/gf2/hunt49.py
@@ -2,24 +2,24 @@
 
 from gf2 import XorshiftAnalyzer
 
-bit_width = 32
-state_size = 3
+bit_width = 64
+state_size = 2
 
 import itertools
 import time
 
-def test(c):
+def test(i, c):
     a = XorshiftAnalyzer(state_size=state_size, bit_width=bit_width)
 
-    # XSAdd (but with state_size=3)
+    # Computes the next state of our Xorshift generator
     def next_state_func(s):
-        t = s[0]
-        t ^= a.shl(t, c[0])
-        t ^= a.shr(t, c[1])
-        t ^= a.shl(s[2], c[2])
-        s[0] = s[1]
-        s[1] = s[2]
-        s[2] = t
+        si0 = s[i[0]]
+        si1 = s[i[1]]
+        si2 = s[i[2]]
+        si3 = s[i[3]]
+
+        s[0] ^= a.shl(si0, c[0]) ^ a.shr(si1, c[1])
+        s[1] ^= a.shl(si2, c[2]) ^ a.shr(si3, c[3])
 
         return s
 
@@ -29,8 +29,9 @@ def test(c):
 if __name__ == "__main__":
     print(f"--- BEGIN: (bit_width={bit_width}, state_size={state_size})", flush=True)
     start = time.time()
-    for c in itertools.product(range(bit_width), repeat=3):
-        if test(c):
-            print(f"{c}: OK", flush=True)
+    for i in itertools.product(range(2), repeat=4):
+        for c in itertools.product(range(bit_width), repeat=4):
+            if test(i, c):
+                print(f"{i}{c}: OK", flush=True)
     elapsed = time.time() - start
     print(f"--- END: (elapsed: {elapsed:.2f} seconds)", flush=True)
