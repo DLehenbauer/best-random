902c4460576e42e254f4b97982c53eded26de5ff
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2025-09-06 12:42:20 -0700

    refine seeding in common.h (wip)

--- tools/TestU01/common.h
+++ tools/TestU01/common.h
@@ -4,8 +4,11 @@
 #include <time.h>
 #include <unistd.h>
 
-static inline uint32_t rot32(uint32_t v, uint32_t k) { k &= 31; return (v << k) | (v >> (32 - k)); }
-static inline uint64_t rot64(uint64_t v, uint64_t k) { k &= 63; return (v << k) | (v >> (64 - k)); }
+// Modern GCC/CLang reduce these to a single instruction on x86/x64.
+static inline uint32_t rol32(uint32_t v, uint32_t k) { k &= 31; return (v << k) | (v >> (32 - k)); }
+static inline uint64_t rol64(uint64_t v, uint64_t k) { k &= 63; return (v << k) | (v >> (64 - k)); }
+static inline uint32_t ror32(uint32_t v, uint32_t k) { k &= 31; return (v >> k) | (v << (32 - k)); }
+static inline uint64_t ror64(uint64_t v, uint64_t k) { k &= 63; return (v >> k) | (v << (64 - k)); }
 
 static inline uint32_t reverse32(uint32_t v)
 {
@@ -13,20 +16,29 @@ static inline uint32_t reverse32(uint32_t v)
     v = ((v >> 2) & 0x33333333) | ((v & 0x33333333) << 2);
     v = ((v >> 4) & 0x0F0F0F0F) | ((v & 0x0F0F0F0F) << 4);
     v = ((v >> 8) & 0x00FF00FF) | ((v & 0x00FF00FF) << 8);
-    return rot32(v, 16);
+    return rol32(v, 16);
+}
+
+static inline uint64_t reverse64(uint64_t v)
+{
+    v = ((v >>  1) & 0x5555555555555555) | ((v & 0x5555555555555555) << 1);
+    v = ((v >>  2) & 0x3333333333333333) | ((v & 0x3333333333333333) << 2);
+    v = ((v >>  4) & 0x0F0F0F0F0F0F0F0F) | ((v & 0x0F0F0F0F0F0F0F0F) << 4);
+    v = ((v >>  8) & 0x00FF00FF00FF00FF) | ((v & 0x00FF00FF00FF00FF) << 8);
+    v = ((v >> 16) & 0x0000FFFF0000FFFF) | ((v & 0x0000FFFF0000FFFF) << 16);
+    return rol64(v, 32);
 }
 
 // Adapted from SplitMix64:
 // (See: https://prng.di.unimi.it/splitmix64.c)
 static inline uint64_t mix64(uint64_t a, uint64_t b) {
-    a ^= b * 0x9e3779b97f4a7c15ULL;
+    a += b * 0x9e3779b97f4a7c15ULL;
 	a = (a ^ (a >> 30)) * 0xbf58476d1ce4e5b9ULL;
 	a = (a ^ (a >> 27)) * 0x94d049bb133111ebULL;
 	return a ^ (a >> 31);
 }
 
-static inline uint64_t us(void) 
-{
+static inline uint64_t us(void) {
     // Get the current time in microseconds since the epoch
     struct timeval tv;
     gettimeofday(&tv, NULL);
@@ -39,11 +51,11 @@ static inline void init_seed(uint64_t seed) {
     if (seed == 0) {
         seed = mix64(
             mix64(
-                (uint64_t) time(NULL),          // Current time
-                (uint64_t) getpid()),           // Process ID
+                (uint64_t) getpid(),            // Process ID
+                (uint64_t) time(NULL)),         // Current time
             mix64(
-                (uint64_t)(uintptr_t) &seed,    // Address of seed
-                (uint64_t) us()));              // Microseconds since epoch
+                (uint64_t) us(),                // Microseconds since epoch
+                (uint64_t)(uintptr_t) &seed));  // Address of seed
     }
 
     wyrand_state = seed;
@@ -55,5 +67,6 @@ static inline uint64_t seed(void) {
     __uint128_t a = wyrand_state;
     __uint128_t b = a ^ 0x8bb84b93962eacc9ull;
     __uint128_t m = a * b;
+    
     return (m >> 64) ^ (uint64_t) m;
 }
