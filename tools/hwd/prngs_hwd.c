506c1d2a20a041cb74f7de1b729595d522c7c27b
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2025-10-24 18:21:48 -0700

    hwd: build hwd32 and hwd64

--- tools/hwd/prngs_hwd.c
+++ tools/hwd/prngs_hwd.c
@@ -2,12 +2,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static inline uint64_t next() {
-    uint64_t u64;
-    size_t elements_read = fread(&u64, sizeof(u64), 1, stdin);
+#if HWD_PRNG_BITS == 64
+typedef uint64_t rng_out_t;
+#else
+typedef uint32_t rng_out_t;
+#endif
+
+static inline rng_out_t next() {
+    rng_out_t output;
+    size_t elements_read = fread(&output, sizeof(output), 1, stdin);
     if (elements_read != 1) {
         fprintf(stderr, "EOF on stdin.\n");
         exit(1);
     }
-    return u64;
+    return output;
 }
